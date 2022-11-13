d5de94f0c92935b189564cd7111139607edfd186
Aza Maulana <azamaulanaaa@gmail.com>
2022-11-13 09:40:23 +0700

    test: add benchmark
    - add test for ghmm fit
    - benchmark for BaseModel and Model
    - implement fixture of pytest

--- tests/hmm/model/ghmm_test.py
+++ tests/hmm/model/ghmm_test.py
@@ -1,41 +1,94 @@
-import unittest
 import numpy as np
+import numpy.typing as npt
+import pytest
 from utils import SMAPE
 from hmm.model.ghmm import Model, BaseModel
 
-class GHMM(unittest.TestCase):
-    tolerance = 1e-10
+class Args(object):
+    T: int
+    N: int
 
+    O: npt.NDArray
+    A: npt.NDArray
+    means: npt.NDArray
+    variances: npt.NDArray
+    startprobs: npt.NDArray
+    min_variance: np.float_
+
+    basemodel: BaseModel
+    model: Model
+
+    frames: npt.NDArray
+    alpha: npt.NDArray
+    beta: npt.NDArray
+    xi: npt.NDArray
+    gamma: npt.NDArray
+
+    def __init__(self, O: npt.NDArray, A: npt.NDArray, means: npt.NDArray, variances: npt.NDArray, startprobs: npt.NDArray, tol: np.float_, niter: np.int_, min_variance: np.float_):
+        self.O = O.copy()
+        self.A = A.copy()
+        self.means = means.copy()
+        self.variances = variances.copy()
+        self.startprobs = startprobs.copy()
+        self.tol = tol
+        self.niter = niter
+        self.min_variance = min_variance
+
+        self.pre_calculate()
+
+    def pre_calculate(self):
+        (self.T, ) = self.O.shape
+        (self.N, ) = self.means.shape
+
+        args = (self.A, self.means, self.variances, self.startprobs)
+        self.basemodel = BaseModel(*args)
+        self.model = Model(*args)
+
+        self.frames = self.basemodel.Frames(self.O, self.means, self.variances)
+        self.alpha = self.basemodel.Forward(self.A, self.frames, self.startprobs)
+        self.beta = self.basemodel.Backward(self.A, self.frames)
+
+        self.xi = self.basemodel.Xi(self.A, self.frames, self.alpha, self.beta)
+        self.gamma = self.basemodel.Gamma(self.alpha, self.beta)
+
+@pytest.fixture
+def args():
     T = 4
     N = 3
+
     O = np.array(range(T), dtype=np.float_)
     A = np.ones((N, N), dtype=np.float_) / N
-    means = np.array(range(N), dtype=np.float_)
-    variances = np.array(range(1, N+1), dtype=np.float_)
+    means = np.zeros((N, ), dtype=np.float_)
+    variances = np.ones((N, ), dtype=np.float_) / N
     startprobs = np.ones((N), dtype=np.float_) / N
+
+    tol = np.float_(1e-5)
+    niter = np.int_(100)
     min_variance = np.float_(1e-2)
 
-    def model(self):
-        testmodel = BaseModel(self.A, self.means, self.variances, self.startprobs, self.min_variance)
-        model = Model(self.A, self.means, self.variances, self.startprobs, self.min_variance)
-        return(testmodel, model)
+    args = Args(O, A, means, variances, startprobs, tol, niter, min_variance)
+    return args
 
-    def test_Frames(self):
-        (testmodel, model) = self.model()
-        args = (self.O, self.means, self.variances)
-        smape = SMAPE(testmodel.Frames(*args), model.Frames(*args))
-        self.assertLessEqual(smape, self.tolerance)
-    
-    def test_RestimateB(self):
-        (testmodel, model) = self.model()
-        frames = testmodel.Frames(self.O, self.means, self.variances)
-        alpha = testmodel.Forward(self.A, frames, self.startprobs)
-        beta = testmodel.Backward(self.A, frames)
-        gamma = testmodel.Gamma(alpha, beta)
-        args = (self.O, gamma)
-        model.RestimateB(*args)
-        testmodel.RestimateB(*args)
-        smapemeans = SMAPE(testmodel.means, model.means)
-        self.assertLessEqual(smapemeans, self.tolerance)
-        smapevariances = SMAPE(testmodel.variances, model.variances)
-        self.assertLessEqual(smapevariances, self.tolerance)
+tolerance = 1e-10
+
+def test_Frames(args):
+    test_args = (args.O, args.means, args.variances)
+    smape = SMAPE(args.basemodel.Frames(*test_args), args.model.Frames(*test_args))
+    assert smape <= tolerance
+
+def test_RestimateB(args):
+    test_args = (args.O, args.gamma)
+    args.model.RestimateB(*test_args)
+    args.basemodel.RestimateB(*test_args)
+    smape_means = SMAPE(args.basemodel.means, args.model.means)
+    assert smape_means <= tolerance
+    smape_variances = SMAPE(args.basemodel.variances, args.model.variances)
+    assert smape_variances <= tolerance
+
+pytestmark = pytest.mark.benchmark(group = "Gaussian Hidden Markov Model")
+
+def test_BaseModel(args, benchmark):
+    benchmark(args.basemodel.Fit, args.O, args.tol, args.niter)
+
+def test_Model(args, benchmark):
+    benchmark(args.model.Fit, args.O, args.tol, args.niter)
--- tests/hmm/model/hmm_test.py
+++ tests/hmm/model/hmm_test.py
@@ -1,119 +1,141 @@
-import unittest
 import numpy as np
+import numpy.typing as npt
+import pytest
 from utils import SMAPE
 from hmm.model.hmm import Model, BaseModel
 
-class HMM(unittest.TestCase):
-    tolerance = 1e-10
+class Args(object):
+    T: int
+    N: int
+    K: int
 
+    O: npt.NDArray
+    A: npt.NDArray
+    V: npt.NDArray
+    B: npt.NDArray
+    startprobs: npt.NDArray
+    
+    basemodel: BaseModel
+    model: Model
+
+    frames: npt.NDArray
+    alpha: npt.NDArray
+    beta: npt.NDArray
+    xi: npt.NDArray
+    gamma: npt.NDArray
+
+    def __init__(self, O: npt.NDArray, A: npt.NDArray, V: npt.NDArray, B: npt.NDArray, startprobs: npt.NDArray, tol: np.float_, niter: np.int_):
+        self.O = O.copy()
+        self.A = A.copy()
+        self.V = V.copy()
+        self.B = B.copy()
+        self.startprobs = startprobs.copy()
+        self.tol = tol
+        self.niter = niter
+
+        self.pre_calculate()
+
+    def pre_calculate(self):
+        (self.T, ) = self.O.shape
+        (self.N, self.K) = self.B.shape
+
+        args = (self.A, self.V, self.B, self.startprobs)
+        self.basemodel = BaseModel(*args)
+        self.model = Model(*args)
+
+        self.frames = self.basemodel.Frames(self.O, self.V, self.B)
+        self.alpha = self.basemodel.Forward(self.A, self.frames, self.startprobs)
+        self.beta = self.basemodel.Backward(self.A, self.frames)
+
+        self.xi = self.basemodel.Xi(self.A, self.frames, self.alpha, self.beta)
+        self.gamma = self.basemodel.Gamma(self.alpha, self.beta)
+
+@pytest.fixture
+def args():
     T = 4
     N = 3
     K = 5
+
     O = np.array(range(T), dtype=np.float_)
-    V = np.array(range(K))
     A = np.ones((N, N), dtype=np.float_) / N
+    V = np.array(range(K))
     B = np.ones((N, K), dtype=np.float_) / N
     startprobs = np.ones((N), dtype=np.float_) / N
+
     tol = np.float_(1e-5)
     niter = np.int_(100)
 
-    def model(self):
-        testmodel = BaseModel(self.A, self.V, self.B, self.startprobs)
-        model = Model(self.A, self.V, self.B, self.startprobs)
-        return(testmodel, model)
+    args = Args(O, A, V, B, startprobs, tol, niter)
+    return args
 
-    def test_Frames(self):
-        (testmodel, model) = self.model()
-        args = (self.O, self.V, self.B)
-        smape = SMAPE(testmodel.Frames(*args), model.Frames(*args))
-        self.assertLessEqual(smape, self.tolerance)
+tolerance = 1e-10
 
-    def test_Forward(self):
-        (testmodel, model) = self.model()
-        frames = testmodel.Frames(self.O, self.V, self.B)
-        args = (self.A, frames, self.startprobs)
-        smape = SMAPE(testmodel.Forward(*args), model.Forward(*args))
-        self.assertLessEqual(smape, self.tolerance)
+def test_Frames(args):
+    test_args = (args.O, args.V, args.B)
+    smape = SMAPE(args.basemodel.Frames(*test_args), args.model.Frames(*test_args))
+    assert smape <= tolerance
 
-    def test_Backward(self):
-        (testmodel, model) = self.model()
-        frames = testmodel.Frames(self.O, self.V, self.B)
-        args = (self.A, frames)
-        smape = SMAPE(testmodel.Backward(*args), model.Backward(*args))
-        self.assertLessEqual(smape, self.tolerance)
+def test_Forward(args):
+    test_args = (args.A, args.frames, args.startprobs)
+    smape = SMAPE(args.basemodel.Forward(*test_args), args.model.Forward(*test_args))
+    assert smape <= tolerance
 
-    def test_Likelihood(self):
-        (testmodel, model) = self.model()
-        frames = testmodel.Frames(self.O, self.V, self.B)
-        alpha = testmodel.Forward(self.A, frames, self.startprobs)
-        args = (alpha, )
-        smape = SMAPE(np.array([testmodel.Likelihood(*args)]), np.array([model.Likelihood(*args)]))
-        self.assertLessEqual(smape, self.tolerance)
+def test_Backward(args):
+    test_args = (args.A, args.frames)
+    smape = SMAPE(args.basemodel.Backward(*test_args), args.model.Backward(*test_args))
+    assert smape <= tolerance
 
-    def test_Gamma(self):
-        (testmodel, model) = self.model()
-        frames = testmodel.Frames(self.O, self.V, self.B)
-        alpha = testmodel.Forward(self.A, frames, self.startprobs)
-        beta = testmodel.Backward(self.A, frames)
-        args = (alpha, beta)
-        smape = SMAPE(testmodel.Gamma(*args), model.Gamma(*args))
-        self.assertLessEqual(smape, self.tolerance)
+def test_Likelihood(args):
+    test_args = (args.alpha, )
+    smape = SMAPE(np.array([args.basemodel.Likelihood(*test_args)]), np.array([args.model.Likelihood(*test_args)]))
+    assert smape <= tolerance
 
-    def test_Xi(self):
-        (testmodel, model) = self.model()
-        frames = testmodel.Frames(self.O, self.V, self.B)
-        alpha = testmodel.Forward(self.A, frames, self.startprobs)
-        beta = testmodel.Backward(self.A, frames)
-        args = (self.A, frames, alpha, beta)
-        smape = SMAPE(testmodel.Xi(*args), model.Xi(*args))
-        self.assertLessEqual(smape, self.tolerance)
+def test_Gamma(args):
+    test_args = (args.alpha, args.beta)
+    smape = SMAPE(args.basemodel.Gamma(*test_args), args.model.Gamma(*test_args))
+    assert smape <= tolerance
 
-    def test_RestimateA(self):
-        (testmodel, model) = self.model()
-        frames = testmodel.Frames(self.O, self.V, self.B)
-        alpha = testmodel.Forward(self.A, frames, self.startprobs)
-        beta = testmodel.Backward(self.A, frames)
-        xi = testmodel.Xi(self.A, frames, alpha, beta)
-        gamma = testmodel.Gamma(alpha, beta)
-        args = (xi, gamma)
-        model.RestimateA(*args)
-        testmodel.RestimateA(*args)
-        smape = SMAPE(testmodel.A, model.A)
-        self.assertLessEqual(smape, self.tolerance)
-    
-    def test_RestimateB(self):
-        (testmodel, model) = self.model()
-        frames = testmodel.Frames(self.O, self.V, self.B)
-        alpha = testmodel.Forward(self.A, frames, self.startprobs)
-        beta = testmodel.Backward(self.A, frames)
-        gamma = testmodel.Gamma(alpha, beta)
-        args = (self.O, gamma)
-        model.RestimateB(*args)
-        testmodel.RestimateB(*args)
-        smape = SMAPE(testmodel.B, model.B)
-        self.assertLessEqual(smape, self.tolerance)
+def test_Xi(args):
+    test_args = (args.A, args.frames, args.alpha, args.beta)
+    smape = SMAPE(args.basemodel.Xi(*test_args), args.model.Xi(*test_args))
+    assert smape <= tolerance
 
-    def test_RestimateStartProbs(self):
-        (testmodel, model) = self.model()
-        frames = testmodel.Frames(self.O, self.V, self.B)
-        alpha = testmodel.Forward(self.A, frames, self.startprobs)
-        beta = testmodel.Backward(self.A, frames)
-        gamma = testmodel.Gamma(alpha, beta)
-        args = (gamma, )
-        model.RestimateStartProbs(*args)
-        testmodel.RestimateStartProbs(*args)
-        smape = SMAPE(testmodel.startprobs, model.startprobs)
-        self.assertLessEqual(smape, self.tolerance)
+def test_RestimateA(args):
+    test_args = (args.xi, args.gamma)
+    args.model.RestimateA(*test_args)
+    args.basemodel.RestimateA(*test_args)
+    smape = SMAPE(args.basemodel.A, args.model.A)
+    assert smape <= tolerance
 
-    def test_Fit(self):
-        (testmodel, model) = self.model()
-        args = (self.O, self.tol, self.niter)
-        model.Fit(*args)
-        testmodel.Fit(*args)
-        smapeA = SMAPE(testmodel.A, model.A)
-        self.assertLessEqual(smapeA, self.tolerance)
-        smapeB = SMAPE(testmodel.B, model.B)
-        self.assertLessEqual(smapeB, self.tolerance)
-        smapestartprobs = SMAPE(testmodel.startprobs, model.startprobs)
-        self.assertLessEqual(smapestartprobs, self.tolerance)
+def test_RestimateB(args):
+    test_args = (args.O, args.gamma)
+    args.model.RestimateB(*test_args)
+    args.basemodel.RestimateB(*test_args)
+    smape = SMAPE(args.basemodel.B, args.model.B)
+    assert smape <= tolerance
 
+def test_RestimateStartProbs(args):
+    test_args = (args.gamma, )
+    args.model.RestimateStartProbs(*test_args)
+    args.basemodel.RestimateStartProbs(*test_args)
+    smape = SMAPE(args.basemodel.startprobs, args.model.startprobs)
+    assert smape <= tolerance
+
+def test_Fit(args):
+    test_args = (args.O, args.tol, args.niter)
+    args.model.Fit(*test_args)
+    args.basemodel.Fit(*test_args)
+    smape_A = SMAPE(args.basemodel.A, args.model.A)
+    assert smape_A <= tolerance
+    smape_B = SMAPE(args.basemodel.B, args.model.B)
+    assert smape_B <= tolerance
+    smape_startprobs = SMAPE(args.basemodel.startprobs, args.model.startprobs)
+    assert smape_startprobs <= tolerance
+
+pytestmark = pytest.mark.benchmark(group = "Hidden Markov Model")
+
+def test_BaseModel(args, benchmark):
+    benchmark(args.basemodel.Fit, args.O, args.tol, args.niter)
+
+def test_Model(args, benchmark):
+    benchmark(args.model.Fit, args.O, args.tol, args.niter)
